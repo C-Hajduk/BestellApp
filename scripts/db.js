44f4220ed415ee4cb0c39047a4005621e2cb5400
C.Hajduk <christian.hajduk@outlook.de>
2025-09-15 16:28:32 +0200

    add basic structure

---
+++ scripts/db.js
@@ -0,0 +1,37 @@
+let myDishes = [
+  {
+    name: "Pizza Margherita",
+    preis: 8.5,
+    description:
+      "Klassische Pizza mit Tomatensauce, Mozzarella und frischem Basilikum auf knusprigem Teig",
+    amount: 0,
+  },
+  {
+    name: "Pizza Salami",
+    preis: 9.9,
+    description:
+      "Herzhafte Pizza mit Tomatensauce, Mozzarella und würziger Salami, ein echter Klassiker",
+    amount: 0,
+  },
+  {
+    name: "Cheeseburger Deluxe",
+    preis: 11.5,
+    description:
+      "Saftiger Rindfleisch-Patty mit Cheddar-Käse, Salat, Tomate, Zwiebeln und hausgemachter Burger-Sauce im Brioche-Brötchen",
+    amount: 0,
+  },
+  {
+    name: "Pizza Hawaii",
+    preis: 10.5,
+    description:
+      "Beliebte Pizza-Kreation mit Tomatensauce, Mozzarella, saftigem Schinken und süßer Ananas",
+    amount: 0,
+  },
+  {
+    name: "Döner Kebab Teller",
+    preis: 12.9,
+    description:
+      "Würziges Dönerfleisch vom Spieß serviert mit Reis, frischem Salat, Gurken, Tomaten und Knoblauch-Joghurt-Sauce",
+    amount: 0,
+  },
+];
