9f2e509015b59889a2c2645ba0fd82e4b0562dcb
C.Hajduk <christian.hajduk@outlook.de>
2025-09-15 11:18:12 +0200

    first commit

---
+++ db.js
@@ -0,0 +1,8 @@
+const cartItems = new Map();
+        const menuItems = {
+            'pizza-krabben': { name: 'Pizza Krabben', price: 8.50, description: 'mit Krabben und Peperoni' },
+            'pizza-margherita': { name: 'Pizza Margherita', price: 5.90, description: 'mit Mozzarella' },
+            'pizza-diavolo': { name: 'Pizza Diavolo (scharf)', price: 8.50, description: 'mit Salami, Zwiebeln, Peperoni und Knoblauch' },
+            'pizzabrötchen': { name: 'Pizzabrötchen', price: 4.20, description: 'mit Käse und Knoblauch' }
+            'pizza-newyork': { name: 'Pizza New York', price: 17.40, description: 'mit Knoblauchsauce, Schinken, milden Peperonie, frischen Champignons, Mozzarellea & echtem Gouda-Käse' }
+        };
